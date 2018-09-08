6dfc5a57f2e339bc4355dd136d4f8dcf4467a46c
Anton <anton.komar2979@gmail.com>
2018-09-09 01:02:18 +0300

    Add Connector

---
+++ XTest2Customize/Connector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace XTest2Customize
+{
+    class Connector
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private static NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+
+        public static void Connect()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void Disconnect()
+        {
+            conn.Close();
+        }
+    }
+}
